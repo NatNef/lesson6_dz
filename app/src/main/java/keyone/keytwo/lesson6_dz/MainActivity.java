b6f0138011af00265b4eb3fa865271836bf936fa
Natalia <nef.n@list.ru>
2021-07-14 13:47:15 +0300

    Проверка

--- app/src/main/java/keyone/keytwo/lesson6_dz/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson6_dz/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_note);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, NoteFragment.newInstance()).commit();
     }
 }
+
--- app/src/main/java/keyone/keytwo/lesson6_dz/NoteFragment.java
+++ app/src/main/java/keyone/keytwo/lesson6_dz/NoteFragment.java
@@ -10,39 +10,12 @@ import android.view.ViewGroup;
 
 
 public class NoteFragment extends Fragment {
-//
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public NoteFragment() {
-//        // Required empty public constructor
-//    }
-//
-   public static NoteFragment newInstance() {
+
+
+    public static NoteFragment newInstance() {
         NoteFragment fragment = new NoteFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
-    }
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_note, container, false);
     }
 }

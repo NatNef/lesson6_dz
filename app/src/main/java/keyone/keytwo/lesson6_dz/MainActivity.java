1e6d1166e04ff85ff09f2ce2af45c497a3445a43
Natalia <nef.n@list.ru>
2021-07-15 17:46:10 +0300

    добавила верхнее меню

--- app/src/main/java/keyone/keytwo/lesson6_dz/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson6_dz/MainActivity.java
@@ -3,7 +3,10 @@ package keyone.keytwo.lesson6_dz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, FragmentOpenNote.newInstance()).commit();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_menu1:
+                Toast.makeText(MainActivity.this, "Нажали 1 пункт меню", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_menu2:
+                Toast.makeText(MainActivity.this, "Нажали 2 пункт меню", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_menu3:
+                Toast.makeText(MainActivity.this, "Нажали 3 пункт меню", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
